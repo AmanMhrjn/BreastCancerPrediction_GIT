936d3f37fa1cf5737531f759991b253c1cb72c79
rohilprajapati <rohilprajapati@gmail.com>
2023-10-31 23:04:19 +0545

    Login and signup fixed

--- login.php
+++ login.php
@@ -1,79 +1,85 @@
 <?php
-  include 'Config/dbconnection.php';
+include 'Config/dbconnection.php';
 
-  session_start();
+session_start();
 
-    if (isset($_SESSION['username'])) {
-        header("Location: index.php");
-    }
-    
-    if ($_POST) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+if (!isset($_SESSION['id'])) {
+  header("Location: index.php");
+}
 
-        $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-        $result = mysqli_query($conn, $sql);
-        echo $conn -> error;
+if ($_POST) {
+  echo "Working";
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
-        if ($result->num_rows != 0) {
-            $user = mysqli_fetch_assoc($result);
-            $_SESSION['id'] = $user['Id'];
-            header("Location: predict.php");
-        } else {
-            echo "<script>alert('Username or Password is Wrong.')</script>";
-        }
-    }
+  $sql = "SELECT * FROM users WHERE username='$username'";
+
+  $result = mysqli_query($conn, $sql);
+  echo $conn->error;
+  echo mysqli_num_rows($result);
+  if (mysqli_num_rows($result) != 0) {
+    $user = mysqli_fetch_assoc($result);
+    $_SESSION['id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
+    header("Location: predict.php");
+  } else {
+    echo "<script>alert('Username not Found.')</script>";
+  }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BCP || Login</title>
-    <link rel="stylesheet" href="/CSS/style.css" />
 
-    <script src="/JS/script.js"></script>
-  </head>
-  <body>
-    <nav>
-      <div class="logoname">Breast Cancer Prediction</div>
-      <ul>
-        <li>
-          <a href="index.php" >Home</a>
-        </li>
-        <li>
-          <a href="predict.php">Predict</a>
-        </li>
-        <li>
-          <a href="login.php" class="active">Login</a>
-        </li>
-      </ul>
-    </nav>
-    <div class="maincontainer">
-      <div class="loginBox">
-        <div class="loginHeader">
-          <p>Login</p>
-        </div>
-        <form action="#" method="post" id="login_form" onsubmit="event.preventDefault(); validateLogin()">
-          <label for="username">Username:</label> <br>
-          <input placeholder="Enter username" class="input" name="username" id="user-name" type="text"> <br>
-          <div class="error" id="username-error"></div>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>BCP || Login</title>
+  <link rel="stylesheet" href="/CSS/style.css" />
 
-          <label for="password">Password:</label> <br>
-          <input placeholder="Enter password" class="input" name="password" id="pass" type="password"> <br>
-          <div class="error" id="password-error"></div>
+  <script src="/JS/script.js"></script>
+</head>
 
-          <p class="signupmsg">
-            Don't have an account? <a href="signup.php" class="signuupLink">Signup</a>
-          </p>
-
-          <input type="submit" class="loginbtn" value="Login"> 
-          <!-- <button class="loginbtn" >Login</button> -->
-        </form>
+<body>
+  <nav>
+    <div class="logoname">Breast Cancer Prediction</div>
+    <ul>
+      <li>
+        <a href="index.php">Home</a>
+      </li>
+      <li>
+        <a href="predict.php">Predict</a>
+      </li>
+      <li>
+        <a href="login.php" class="active">Login</a>
+      </li>
+    </ul>
+  </nav>
+  <div class="maincontainer">
+    <div class="loginBox">
+      <div class="loginHeader">
+        <p>Login</p>
       </div>
+      <form action="#" method="post" id="login_form" onsubmit="event.preventDefault(); validateLogin()">
+        <label for="username">Username:</label> <br>
+        <input placeholder="Enter username" class="input" name="username" id="user-name" type="text"> <br>
+        <div class="error" id="username-error"></div>
+
+        <label for="password">Password:</label> <br>
+        <input placeholder="Enter password" class="input" name="password" id="pass" type="password"> <br>
+        <div class="error" id="password-error"></div>
+
+        <p class="signupmsg">
+          Don't have an account? <a href="signup.php" class="signuupLink">Signup</a>
+        </p>
+
+        <input type="submit" class="loginbtn" value="Login">
+        <!-- <button class="loginbtn" >Login</button> -->
+      </form>
     </div>
-    <footer>
-      <p>Copyright &copy; 2020 | All Rights Reserved by Breast Cancer Prediction</p>
+  </div>
+  <footer>
+    <p>Copyright &copy; 2020 | All Rights Reserved by Breast Cancer Prediction</p>
   </footer>
-  </body>
+</body>
+
 </html>
--- signup.php
+++ signup.php
@@ -1,107 +1,119 @@
 <?php
-    include 'Config/dbconnection.php';
+include 'Config/dbconnection.php';
 
-    if (isset($_SESSION['username'])) {
-        header("Location: login.php");
-    }
+if (!isset($_SESSION['id'])) {
+  header("Location: index.php");
+}
 
-    if($_POST){
-        $fullname = $_POST['fullname'];
-        $username = $_POST['username'];
-        $address = $_POST['address'];
-        $age = $_POST['age'];
-        $gender = $_POST['gender'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        
-            $sql = "SELECT * FROM users WHERE username='$username' AND password='$password' ";
-            $result = mysqli_query($conn, $sql);
-            echo $conn -> error;
-            if ($result->num_rows == 0) {
-                $sql = "INSERT INTO users (fullname,username, address, age, gender, email, password) VALUES ('$fullname','$username', '$address', '$age', '$gender', '$email', '$password')";
-                $result = mysqli_query($conn, $sql);
-                
-                if ($result) {
-                    echo "<script>alert('User Registration Completed.')</script>";
-                    header("Location: login.php");
-                } else {
-                    echo "<script>alert('Something Wrong Went.')</script>";
-                }
-            } else {
-                echo "<script>alert('Email Already Exists.')</script>";
-            }
+if ($_POST) {
+  $fullname = $_POST['fullname'];
+  $username = $_POST['username'];
+  $address = $_POST['address'];
+  $age = $_POST['age'];
+  $gender = $_POST['gender'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  $sql = "SELECT * FROM users WHERE username='$username'";
+  $result = mysqli_query($conn, $sql);
+  echo $conn->error;
+  if (mysqli_num_rows($result) == 0) {
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (fullname,username, address, age, gender, email, password) VALUES ('$fullname','$username', '$address', '$age', '$gender', '$email', '$hash')";
+
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
+      echo "<script>alert('User Registration Completed.')</script>";
+      header("Location: login.php");
+    } else {
+      echo "<script>alert('Something Wrong Went.')</script>";
     }
+  } else {
+    echo "<script>alert('Username Already Exists.')</script>";
+  }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BCP || Signup</title>
-    <link rel="stylesheet" href="/CSS/style.css" />
 
-    <script src="/JS/script.js"></script>
-  </head>
-  <body>
-    <nav>
-      <div class="logoname">Breast Cancer Prediction</div>
-      <ul>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>BCP || Signup</title>
+  <link rel="stylesheet" href="/CSS/style.css" />
+
+  <script src="/JS/script.js"></script>
+</head>
+
+<body>
+  <nav>
+    <div class="logoname">Breast Cancer Prediction</div>
+    <ul>
+      <li>
+        <a href="index.php">Home</a>
+      </li>
+      <li>
+        <a href="predict.php">Predict</a>
+      </li>
+      <?php if ($_SESSION['id']) { ?>
         <li>
-          <a href="index.php" >Home</a>
+          <a href="login.php" class="active"><?= $_SESSION['username'] ?></a>
         </li>
-        <li>
-          <a href="predict.php">Predict</a>
-        </li>
+      <?php } else { ?>
         <li>
           <a href="login.php" class="active">Login</a>
         </li>
-      </ul>
-    </nav>
-    <div class="maincontainersignup">
-      <div class="signupBox">
-        <div class="signupHeader">
-          <p>Signup</p>
-        </div>
-        <form id="signUp_form" method="POST" onsubmit="event.preventDefault(); validateSignup()">
-          <label for="fullname">Full Name:</label> <br>
-          <input placeholder="Enter fullname" class="input" id="full-name" name="fullname" type="text" > <br>
-          <div class="error" id="fullname-error"></div>
-
-          <label for="username">Username:</label> <br> 
-          <input placeholder="Enter username" class="input" id="user-name" name="username" type="text" >  <br>
-          <div class="error" id="username-error"></div>
-          
-          <label for="address">Address:</label> <br> 
-          <input placeholder="Enter address" class="input" id="address" name="address" type="text" >  <br>
-          <div class="error" id="address-error"></div>
-
-          <label for="age">Age:</label> <br> 
-          <input placeholder="Enter your age" class="input" id="age" name="age" type="number" >  <br>
-          <div class="error" id="age-error"></div>
-
-          <label for="gender">Gender:</label> <br> 
-          <input placeholder="Enter your gender Male or Female" id="gender" class="input" name="gender" type="text" >  <br>
-          <div class="error" id="gender-error"></div>
-
-          <label for="email">email:</label> <br> 
-          <input placeholder="Enter password" class="input" id="email" name="email" type="email" >  <br>
-          <div class="error" id="email-error"></div>
-
-          <label for="password">Password:</label> <br> 
-          <input placeholder="Enter password" class="input" id="password" name="password" type="password" >  <br>
-          <div class="error" id="password-error"></div>
-          
-          <p class="loginmsg">
-            Already have an account? <a href="login.php" class="loginlink">Login</a>
-          </p>
-
-          <input type="submit" class="signupbtn" value="Signup">
-          <!-- <button class="signupbtn" name="submit">Signup</button> -->
-        </form>
+      <?php } ?>
+    </ul>
+  </nav>
+  <div class="maincontainersignup">
+    <div class="signupBox">
+      <div class="signupHeader">
+        <p>Signup</p>
       </div>
+      <form id="signUp_form" method="POST" onsubmit="event.preventDefault(); validateSignup()">
+        <label for="fullname">Full Name:</label> <br>
+        <input placeholder="Enter fullname" class="input" id="full-name" name="fullname" type="text"> <br>
+        <div class="error" id="fullname-error"></div>
+
+        <label for="username">Username:</label> <br>
+        <input placeholder="Enter username" class="input" id="user-name" name="username" type="text"> <br>
+        <div class="error" id="username-error"></div>
+
+        <label for="address">Address:</label> <br>
+        <input placeholder="Enter address" class="input" id="address" name="address" type="text"> <br>
+        <div class="error" id="address-error"></div>
+
+        <label for="age">Age:</label> <br>
+        <input placeholder="Enter your age" class="input" id="age" name="age" type="number"> <br>
+        <div class="error" id="age-error"></div>
+
+        <label for="gender">Gender:</label> <br>
+        <input placeholder="Enter your gender Male or Female" id="gender" class="input" name="gender" type="text"> <br>
+        <div class="error" id="gender-error"></div>
+
+        <label for="email">email:</label> <br>
+        <input placeholder="Enter password" class="input" id="email" name="email" type="email"> <br>
+        <div class="error" id="email-error"></div>
+
+        <label for="password">Password:</label> <br>
+        <input placeholder="Enter password" class="input" id="password" name="password" type="password"> <br>
+        <div class="error" id="password-error"></div>
+
+        <p class="loginmsg">
+          Already have an account? <a href="login.php" class="loginlink">Login</a>
+        </p>
+
+        <input type="submit" class="signupbtn" value="Signup">
+        <!-- <button class="signupbtn" name="submit">Signup</button> -->
+      </form>
     </div>
-    <footer>
-        <p>Copyright &copy; 2020 | All Rights Reserved by Breast Cancer Prediction</p>
-    </footer>
-  </body>
+  </div>
+  <footer>
+    <p>Copyright &copy; 2020 | All Rights Reserved by Breast Cancer Prediction</p>
+  </footer>
+</body>
+
 </html>
